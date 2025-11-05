8d69c3dfcc619d803c1351f9ebc296372bc7f4f7
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-05 13:51:09 +0100

    chore: apply swift-format with 4-space indentation

--- Sources/StructuredQueriesPostgresMacros/Internal/VariableDeclSyntax.swift
+++ Sources/StructuredQueriesPostgresMacros/Internal/VariableDeclSyntax.swift
@@ -1,54 +1,54 @@
 import SwiftSyntax
 
 extension VariableDeclSyntax {
-  func hasMacroApplication(_ name: String) -> Bool {
-    for attribute in attributes {
-      switch attribute {
-      case .attribute(let attr):
-        if attr.attributeName.tokens(viewMode: .all).map({ $0.tokenKind }) == [
-          .identifier(name)
-        ] {
-          return true
+    func hasMacroApplication(_ name: String) -> Bool {
+        for attribute in attributes {
+            switch attribute {
+            case .attribute(let attr):
+                if attr.attributeName.tokens(viewMode: .all).map({ $0.tokenKind }) == [
+                    .identifier(name)
+                ] {
+                    return true
+                }
+            default:
+                break
+            }
         }
-      default:
-        break
-      }
+        return false
     }
-    return false
-  }
 
-  var isComputed: Bool {
-    for binding in bindings {
-      switch binding.accessorBlock?.accessors {
-      case .getter:
-        return true
-      case .accessors(let accessors):
-        for accessor in accessors {
-          if accessor.accessorSpecifier.tokenKind == .keyword(.get) {
-            return true
-          }
+    var isComputed: Bool {
+        for binding in bindings {
+            switch binding.accessorBlock?.accessors {
+            case .getter:
+                return true
+            case .accessors(let accessors):
+                for accessor in accessors {
+                    if accessor.accessorSpecifier.tokenKind == .keyword(.get) {
+                        return true
+                    }
+                }
+            default:
+                continue
+            }
         }
-      default:
-        continue
-      }
+        return false
     }
-    return false
-  }
 
-  var isStatic: Bool {
-    modifiers.contains { modifier in
-      modifier.name.tokenKind == .keyword(.static)
+    var isStatic: Bool {
+        modifiers.contains { modifier in
+            modifier.name.tokenKind == .keyword(.static)
+        }
     }
-  }
 
-  func removingAccessors() -> Self {
-    var variable = self
-    variable.bindings = PatternBindingListSyntax(
-      variable.bindings.map {
-        var binding = $0
-        binding.accessorBlock = nil
-        return binding
-      })
-    return variable
-  }
+    func removingAccessors() -> Self {
+        var variable = self
+        variable.bindings = PatternBindingListSyntax(
+            variable.bindings.map {
+                var binding = $0
+                binding.accessorBlock = nil
+                return binding
+            })
+        return variable
+    }
 }
